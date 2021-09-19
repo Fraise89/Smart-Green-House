0bce18623e44978403b6e89961a84e0ee9113973
Fraise89 <megafraise89@gmail.com>
2021-09-19 12:04:20 +0200

    ajout de fonctionnalité
    Création des fonctions:
    - fill_an_object
    - get_form
    - get_gravity_point
    -isolate_object

--- Raspberry/caméra/traitement d'image/function.py
+++ Raspberry/caméra/traitement d'image/function.py
@@ -3,6 +3,7 @@ Fonction lié au traitement d'image
 """
 import numpy as np
 import math
+
 #obtention d'un pixel en son équivalent gris
 def get_gray_pix(image, pixel):
     pixel = image.getpixel(pixel)
@@ -53,17 +54,18 @@ def get_seuil(image):
     print('End of the treshold processing!')
     return seuil
 #binarisation d'une image
+#!!! Attention, certaine fois la binarisation est inversé
 def gray2bin(image):
     print('Binarisation processing... ')
     seuil = get_seuil(image)
     definition = image.size
-    new_image = np.zeros(definition)
+    new_image = np.zeros((definition[1], definition[0]))
     for y in range(definition[1]):
         for x in range(definition[0]):
             if get_gray_pix(image, (x, y)) > seuil:
-                new_image[x][y] = 0
+                new_image[y][x] = 1
             else:
-                new_image[x][y] = 1
+                new_image[y][x] = 0
     print('End of the binarisation processing!')
     return new_image
 #errodation d'une image binarisé
@@ -108,6 +110,187 @@ def dilate(image, intensity):
         print('done!')
     print('End of the dilatation processing!')
     return new_image
-#ne fait rien mdr
+#remplissage d'une forme d'une image à partir d'un pixel
+#obselette / à refaire
+def fill_an_object(color, image, image_group):
+    definition = image.shape
+    for y in range(definition[1]):
+        for x in range(definition[0]):
+            if image_group[x][y] == color:
+                #4 cotés
+                if y+1 < definition[1]:
+                    if image[x][y+1] == 1:
+                        image_group[x][y+1]= color
+                if y-1 >= 0:
+                    if image[x][y-1] == 1:
+                        image_group[x][y-1] = color
+                if x+1 < definition[0]:
+                    if image[x+1][y] == 1:
+                        image_group[x+1][y] = color
+                if x-1 >= 0:
+                    if image[x-1][y] == 1:
+                        image_group[x-1][y] = color
+                #diagonales
+                if x-1 >= 0 and y-1>= 0:
+                    if image[x-1][y-1] == 1:
+                        image_group[x-1][y-1] = color
+                if x+1 < definition[0] and y-1 >= 0:
+                    if image[x+1][y-1] == 1:
+                        image_group[x+1][y-1] = color
+                if x-1 >= 0 and y+1 < definition[1]:
+                    if image[x-1][y+1] == 1:
+                        image_group[x-1][y+1] = color
+                if x+1 < definition[0] and y+1 < definition[1]:
+                    if image[x+1][y+1] == 1:
+                        image_group[x+1][y+1] = color
+    for y in range(definition[1]):
+        for x in range(definition[0]-1,0,-1):
+            if image_group[x][y] == color:
+                #4 cotés
+                if y+1 < definition[1]:
+                    if image[x][y+1] == 1:
+                        image_group[x][y+1]= color
+                if y-1 >= 0:
+                    if image[x][y-1] == 1:
+                        image_group[x][y-1] = color
+                if x+1 < definition[0]:
+                    if image[x+1][y] == 1:
+                        image_group[x+1][y] = color
+                if x-1 >= 0:
+                    if image[x-1][y] == 1:
+                        image_group[x-1][y] = color
+                #diagonales
+                if x-1 >= 0 and y-1>= 0:
+                    if image[x-1][y-1] == 1:
+                        image_group[x-1][y-1] = color
+                if x+1 < definition[0] and y-1 >= 0:
+                    if image[x+1][y-1] == 1:
+                        image_group[x+1][y-1] = color
+                if x-1 >= 0 and y+1 < definition[1]:
+                    if image[x-1][y+1] == 1:
+                        image_group[x-1][y+1] = color
+                if x+1 < definition[0] and y+1 < definition[1]:
+                    if image[x+1][y+1] == 1:
+                        image_group[x+1][y+1] = color
+    for y in range(definition[1]-1,0,-1):
+        for x in range(definition[0]):
+            if image_group[x][y] == color:
+                #4 cotés
+                if y+1 < definition[1]:
+                    if image[x][y+1] == 1:
+                        image_group[x][y+1]= color
+                if y-1 >= 0:
+                    if image[x][y-1] == 1:
+                        image_group[x][y-1] = color
+                if x+1 < definition[0]:
+                    if image[x+1][y] == 1:
+                        image_group[x+1][y] = color
+                if x-1 >= 0:
+                    if image[x-1][y] == 1:
+                        image_group[x-1][y] = color
+                #diagonales
+                if x-1 >= 0 and y-1>= 0:
+                    if image[x-1][y-1] == 1:
+                        image_group[x-1][y-1] = color
+                if x+1 < definition[0] and y-1 >= 0:
+                    if image[x+1][y-1] == 1:
+                        image_group[x+1][y-1] = color
+                if x-1 >= 0 and y+1 < definition[1]:
+                    if image[x-1][y+1] == 1:
+                        image_group[x-1][y+1] = color
+                if x+1 < definition[0] and y+1 < definition[1]:
+                    if image[x+1][y+1] == 1:
+                        image_group[x+1][y+1] = color
+    for y in range(definition[1]-1,0,-1):
+        for x in range(definition[0]-1,0,-1):
+            if image_group[x][y] == color:
+                #4 côtés
+                if y+1 < definition[1]:
+                    if image[x][y+1] == 1:
+                        image_group[x][y+1] = color
+                if y-1 >= 0:
+                    if image[x][y-1] == 1:
+                        image_group[x][y-1] = color
+                if x+1 < definition[0]:
+                    if image[x+1][y] == 1:
+                        image_group[x+1][y] = color
+                if x-1 >= 0:
+                    if image[x-1][y] == 1:
+                        image_group[x-1][y] = color
+                #4 diagonales
+                if x-1 >= 0 and y-1 >= 0:
+                    if image[x-1][y-1] == 1:
+                        image_group[x-1][y-1] = color
+                if x+1 < definition[0] and y-1 >= 0:
+                    if image[x+1][y-1] == 1:
+                        image_group[x+1][y-1] = color
+                if x-1 >= 0 and y+1 < definition[1]:
+                    if image[x-1][y+1] == 1:
+                        image_group[x-1][y+1] = color
+                if x+1 < definition[0] and y+1 < definition[1]:
+                    if image[x+1][y+1] == 1:
+                        image_group[x+1][y+1] = color
+    return image_group
+#obtention d'une matrice avec tout les objets
+#retourne le nombre d'objets et la matrice d'objets
 def get_form(image):
-    print("lol")
+    print("Object counting processing...")
+    definition = image.shape
+    image_groupe = np.zeros(definition)
+    color = 0
+    stop = 0
+    # regarde si un pixel appartient déja à un objet détécté
+    # ou non
+    while True:
+        for y in range(definition[1]):
+            for x in range(definition[0]):
+                if image[x][y] == 1 and image_groupe[x][y] == 0 and stop == 0:
+                    color += 1
+                    stop = 1
+                    #création d'un pixel dans la matrice des objets trouvé
+                    image_groupe[x][y] = color
+
+        if stop == 0:
+            break
+        stop = 0
+        print('object ',color,'processing...')
+        image_groupe = fill_an_object(color, image, image_groupe)
+    nombre = color
+    print("End of the object counting processing!")
+    return image_groupe,nombre
+#retourne le centre de gravité d'un objet
+def get_gravity_point(image):
+    definition = image.shape
+    xg = definition[0]/2
+    yg = definition[1]/2
+    segment = np.zeros((definition[1],2))
+    for y in range(definition[1]):
+        for x in range(definition[0]):
+            if image[x][y] == 1:
+                segment[y][0] = x
+                for x in range(x,definition[0]):
+                    if image[x][y] == 1:
+                        segment[y][1] += 1
+                    else:
+                        break
+                break
+    sx = 0
+    sy = 0
+    m = 0
+    for i in range(definition[1]):
+        sx += ((segment[i][0]+segment[i][1])**2 - segment[i][0]**2)/2
+        sy += segment[i][1]*((i+1)**2 -i**2)/2
+        m += segment[i][1]
+    if m != 0:
+        xg = sx/m
+        yg = sy/m
+    return round(xg),round(yg)
+#isole un objet de l'image
+def isolate_object(image,color):
+    definition = image.shape
+    new_image = np.zeros(definition)
+    for y in range(definition[1]):
+        for x in range(definition[0]):
+            if image[x][y] == color:
+                new_image[x][y] = 1
+    return new_image
