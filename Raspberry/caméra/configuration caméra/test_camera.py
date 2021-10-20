f2e3ebe408916b394fd49a7c89fb8e4a3c8843a5
Laurie-S <56963780+Laurie-S@users.noreply.github.com>
2021-10-20 08:52:56 +0200

    Delete Raspberry/caméra/configuration caméra directory

--- Raspberry/caméra/configuration caméra/test_camera.py
+++
@@ -1,9 +0,0 @@
-from picamera import PiCamera
-
-from time import sleep
-
-camera = PiCamera()
-camera.start_preview(alpha = 192)
-sleep(1)
-camera.capture("/home/pi/Documents/test camera/pic.png")
-camera.stop_preview()
